b9d2f86e55cb25f44dff74bf31907e724d7cb158
Rserli <riccardoserli.95@gmail.com>
2025-05-07 19:44:56 +0200

    Writedown Logic

--- script/app.js
+++ script/app.js
@@ -1,1 +1,15 @@
 console.log("Biglietto del treno")
+
+// ---- VARIABILI UTILI ----
+// Prezzo al chilometro
+// Percentuale sconto Under18
+// Percentuale sconto Over65
+// ---- INPUT ----
+// chidere età del viaggiatore
+// chiedere qunanti chilometri è lunga la tratta per arrivare a destinazione
+// ---- CALCOLI ----
+// calcolo prezzo PIENO della tratta
+// calcolo prezzo SCONTATO della tratta under 18
+// calcolo prezzo SCONTATO della tratta over 65
+// ---- OUTPUT ----
+// Mostra prezzo finale a seconda dell'età del viaggiatore
