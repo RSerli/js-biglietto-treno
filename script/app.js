9e208f5db89cadee514524f1e368e64a062145b8
Rserli <riccardoserli.95@gmail.com>
2025-05-07 19:54:02 +0200

    First Variables

--- script/app.js
+++ script/app.js
@@ -1,9 +1,12 @@
 console.log("Biglietto del treno")
 
 // ---- VARIABILI UTILI ----
-// Prezzo al chilometro
+// Prezzo biglietto al chilometro
+const CostoChilometrico = 0.21 // Number Float
 // Percentuale sconto Under18
+const ScontoUnder18 = 20 // Number integer
 // Percentuale sconto Over65
+const ScontoOver65 = 40 // Number integer
 // ---- INPUT ----
 // chidere età del viaggiatore
 // chiedere qunanti chilometri è lunga la tratta per arrivare a destinazione
